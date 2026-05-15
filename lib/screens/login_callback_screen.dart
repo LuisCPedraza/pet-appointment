1e349439f28957933aea1b7cc8f850a020f1c9ac
Nicolas_GG <nicolasgg2020@gmail.com>
2026-05-14 21:02:25 -0500

    Añadiendo inicio de sesión con google y con github

---
+++ lib/screens/login_callback_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class LoginCallbackScreen extends StatelessWidget {
+  const LoginCallbackScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
